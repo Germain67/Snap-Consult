cbcf65661c4b7b3c1ff1bee51c75e23ee8bdc25a
Mathieu Cordebard <mathieu.cordebard@al-enterprise.com>
2018-03-24 08:57:45 +0100

    add multilanguage support

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,6 +1,5 @@
 import angular from 'angular'
 import uirouter from '@uirouter/angularjs'
-
 import routes from './app.routes'
 import homeComponent from './homeComponent'
 import secondComponent from './secondComponent/secondComponent'
@@ -8,5 +7,14 @@ import secondComponent from './secondComponent/secondComponent'
 import styles from './app.css'
 
 
-angular.module('snapConsult', [uirouter, homeComponent])
-    .config(routes)
+angular.module('snapConsult', [uirouter, 'pascalprecht.translate', homeComponent])
+    .config(routes);
+
+angular.module('snapConsult').config(['$translateProvider', function ($translateProvider) {
+     $translateProvider.useStaticFilesLoader({
+        prefix: "/resources/languages/",
+        suffix: ".json"
+    });
+    $translateProvider.useSanitizeValueStrategy(null);
+    $translateProvider.preferredLanguage('en');
+}]);
