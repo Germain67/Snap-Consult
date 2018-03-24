98aa21a0fdeecda430278947d0b10b51469234f4
Germain CLAUSS <germain_clauss@yahoo.fr>
2018-03-24 11:55:35 +0100

    Users schema and seeds

--- server/index.js
+++ server/index.js
@@ -13,35 +13,42 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => { console.log('db successfully connected'); });
 
-const kittySchema = mongoose.Schema({
-  name: String,
+const userSchema = mongoose.Schema({
+  login: String,
+  password: String,
+  email: String,
+  firstname: String,
+  lastname: String,
+  phonenumber: String
 });
-const Kitten = mongoose.model('Kitten', kittySchema);
+const Users = mongoose.model('users', userSchema);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.get('/addkitten', (req, res) => {
-  const silence = new Kitten({ name: 'Silence' });
-  console.log(silence.name); // 'Silence'
-  silence.save((err, kit) => {
+app.get('/adduser', (req, res) => {
+  const myuser = new Users({ login: '', password: '', email: '', firstname: '', lastname: '', phonenumber: '' });
+
+  myuser.save((err, resp) => {
     if (err) {
       console.error(err);
+      res.status(500).send(err);
     } else {
-      console.log(kit.name); // 'Silence'
-      res.send('New kitten added !');
+      console.log(resp.firstname);
+      res.status(200).send(resp);
     }
   });
 });
 
-app.get('/kittens', (req, res) => {
-  Kitten.find((err, kittens) => {
+app.get('/users', (req, res) => {
+  Users.find((err, users) => {
     if (err) {
       console.error(err);
+      res.status(500).send(err);
     }
-    res.send(kittens);
+    res.status(200).send(users);
   });
 });
 
